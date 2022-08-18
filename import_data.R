49c5ebf14a7987917221032f99a709c357779f78
matahmet <matahmet@gmail.com>
2022-08-18 17:28:55 +0300

    M3anu

---
+++ import_data.R
@@ -0,0 +1,10 @@
+#install.packages("xslx")
+#library("xlsx")
+#install.packages('xlsx', repos='http://cran.us.r-project.org')
+#install.packages('xlsx', repos='http://cran.us.r-project.org')
+
+#install.packages("readxl")
+library(readxl)
+#read_excel("Path where your Excel file is stored\\File Name.xlsx")
+
+df=read_excel("C:\\Users\\emrehan\\Documents\\GitHub\\Pasha\\All136Data\\Data4_PureData2_with_header.xlsx")
